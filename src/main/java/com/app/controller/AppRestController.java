be627b3c252abcab7883f1f61721faffae5209a5
fhluky <fhluky@gmail.com>
2019-02-23 12:42:34 +0700

    message

--- src/main/java/com/app/controller/AppRestController.java
+++ src/main/java/com/app/controller/AppRestController.java
@@ -15,14 +15,16 @@ public class AppRestController {
 	private Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@PostMapping("/service1")
-	public ResponseBean<AppBean> service1(@RequestBody AppBean appBean) {
+	public ResponseBean<AppBean> service1(@RequestBody AppBean appBean) throws InterruptedException {
 		
 		ResponseBean<AppBean> result = new ResponseBean<>();
-		if("1350100204282".equals(appBean.getNatId())) {
+		if(!"1350100204282".equals(appBean.getNatId())) {
 			result.setStatus("0000");
 		} else {
 			result.setStatus("9999");
 		}
+		
+		Thread.sleep(2000);
 		
 		result.setData(appBean);
 		
