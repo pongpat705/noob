0c8c8c289e5b7855cc6bc9a5daa17442ce68c041
fhluky <fhluky@gmail.com>
2019-02-23 10:28:54 +0700

    message

--- src/main/java/com/app/controller/AppRestController.java
+++ src/main/java/com/app/controller/AppRestController.java
@@ -1,21 +1,29 @@
 package com.app.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.AppBean;
+import com.app.ResponseBean;
 
 @RestController
 public class AppRestController {
 
+	private Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@PostMapping("/service1")
-	public AppBean service1(@RequestBody AppBean appBean) {
+	public ResponseBean<AppBean> service1(@RequestBody AppBean appBean) {
 		
-		System.out.println(appBean.toString());
+		ResponseBean<AppBean> result = new ResponseBean<>();
+		result.setStatus("9999");
+		result.setData(appBean);
 		
-		return appBean;
+		log.info(result.toString());
+		
+		return result;
 	}
 	
 	
